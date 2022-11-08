6203ba8ef7506111aff0e1e2fd204c57933ac9bc
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-11-08 12:15:19 -0500

    done

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 // Replace the value with your NFT contract address
 const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "YOUR_CRYPTODEVS_NFT_CONTRACT_ADDRESS_HERE";
+  "0xf7658beca071323b844999132f16672f2211d460";
 
 module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
